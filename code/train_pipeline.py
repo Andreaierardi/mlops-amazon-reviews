5d0ad2be68413e97f3fe0daf8144e34099baa87d
skyierardia <andrea.ierardi@skytv.it>
2025-10-04 18:29:33 +0200

    add print info in training pipeline

--- code/train_pipeline.py
+++ code/train_pipeline.py
@@ -17,7 +17,10 @@ parser.add_argument("--data", required=True)
 parser.add_argument("--output", required=True)
 args = parser.parse_args()
 
+print("Loading dataset")
 df = pd.read_json(args.data, lines=True)
+print("Dataset shape", df.shape)
+print("Dataset columns", df.columns)
 
 TEXT_COL, LABEL_COL = "text", "sentiment"
 
@@ -32,6 +35,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     df[TEXT_COL], df[LABEL_COL], test_size=0.2, random_state=42, stratify=df[LABEL_COL]
 )
 
+print("Start training")
 pipe = Pipeline([
     ("clean", TextCleaner()),
     ("tfidf", TfidfVectorizer(max_features=5000, ngram_range=(1,2))),
